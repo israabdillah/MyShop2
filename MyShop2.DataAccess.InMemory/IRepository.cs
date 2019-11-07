77db2a2444da3ead2e1f8f417073661cd62e1857
israabdillah <39734378+israabdillah@users.noreply.github.com>
2019-11-07 14:17:42 +0700

    Project Crud To Database

---
+++ MyShop2.DataAccess.InMemory/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using MyShop2.Core.Models;
+
+namespace MyShop2.DataAccess.InMemory
+{
+	public interface IRepository<T> where T : BaseEntity
+	{
+		IQueryable<T> Collection();
+		void Commit();
+		void Delete(string Id);
+		T Find(string Id);
+		void Insert(T t);
+		void Update(T t);
+	}
+}
